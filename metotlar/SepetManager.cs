18d1f6eec11ead15a30c87f4356ebcf37b04a345
guney <guneytaran@gmail.com>
2021-01-21 00:03:26 +0300

    20 Ocak Ders Sonu

---
+++ metotlar/SepetManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace metotlar
+{
+    class SepetManager
+    {
+        //naming convention
+        //syntax
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi! : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi! : " + urunAdi);
+        }
+    }
+}
+
---
+++ metotlar/Urun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace metotlar
+{
+    class Urun
+    {
+        //Property - özellik
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public double Fiyati { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
